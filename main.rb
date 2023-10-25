4d99804356166aa138825cfb2dc3b6d7666e9dfe
Alabhya Jindal <alabhya10@gmail.com>
2023-10-25 12:20:27 +0530

    Init

---
+++ main.rb
@@ -0,0 +1,16 @@
+require 'sqlite3'
+require 'sinatra'
+
+db = SQLite3::Database.new "contacts"
+
+contacts = db.execute <<-SQL 
+  select * from contacts;
+SQL
+
+names = contacts.map { |contact| contact[1] }
+
+get '/' do
+    friends = names.map { | name | "<p>#{name}</p>" }
+    "<h1>Friends</h1>#{friends.join(' ')}"
+end
+  
